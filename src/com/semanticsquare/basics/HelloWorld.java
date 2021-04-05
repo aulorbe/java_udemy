e91640ec61e72a6c42fa96865fd609902e048300
Audrey Lorberfeld <audrey.lorberfeld@shopify.com>
2021-04-05 10:19:20 -0400

    test

---
+++ src/com/semanticsquare/basics/HelloWorld.java
@@ -0,0 +1,5 @@
+class HelloWorld {  // if you have "public" before class, the file name & the class name have to be the same
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!!");
+	}
+}
